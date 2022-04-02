282b44a1b355aeb0c116771debd91bfcb47b1310
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-04-02 19:02:49 +0800

    added other brush functionality

--- script.js
+++ script.js
@@ -3,9 +3,27 @@
 const sketchContainer = document.querySelector("#sketch-container");
 const cleanGrid = document.querySelector(".clean-grid");
 const gridSizeText = document.querySelector("#grid-size-text");
+const blackBrush = document.querySelector("#black-brush");
+const rainbowBrush = document.querySelector("#rainbow-brush");
 
 //cleanliness lol
 const gridBGColor = "rgb(246, 242, 247)";
+const rainbowColors = [
+    "rgb(255,0,0)",
+    "rgb(255,127,0)",
+    "rgb(255,255,0)",
+    "rgb(127,255,0)",
+    "rgb(0,255,0)",
+    "rgb(0,255,127)",
+    "rgb(0,255,255)",
+    "rgb(0,127,255)",
+    "rgb(0,0,255)",
+    "rgb(127,0,255)",
+    "rgb(255,0,255)",
+    "rgb(255,0,127)"
+]
+
+const randomNumber = (int) => Math.floor(Math.random()*int);
 
 function createGridCells() {
     const newDiv = document.createElement("div");
@@ -54,20 +72,44 @@ function newGrid(gridSize) {
 
 function setHoverEventListener() {
     const totalNoOfGrids = sketchContainer.childElementCount
-    for (let i = 0; i < totalNoOfGrids; i++) {
-        sketchContainer.childNodes[i].addEventListener("mouseover", (mouseTarget) => {
-            mouseTarget.target.style.backgroundColor = `rgb(36, 40, 48)`;
-        })
+
+    if (blackBrush.checked) {
+        for (let i = 0; i < totalNoOfGrids; i++) {
+            sketchContainer.childNodes[i].addEventListener("mouseover", (mouseTarget) => {
+                mouseTarget.target.style.backgroundColor = `rgb(36, 40, 48)`;
+            })
+        }
     }
+
+    if (rainbowBrush.checked) {
+        const rainbowColorNumber = rainbowColors.length
+        for (let i = 0; i < totalNoOfGrids; i++) {
+            sketchContainer.childNodes[i].addEventListener("mouseover", (mouseTarget) => {
+                mouseTarget.target.style.backgroundColor = rainbowColors[randomNumber(rainbowColorNumber)];
+            })
+        }
+    }
+
 }
 
 const setEventListeners = () => {
     cleanGrid.addEventListener("click", clearGrid)
+
     gridSizeText.addEventListener("keypress", (textfield) => {
         if (textfield.key === 'Enter') {
             gridSize = parseInt(textfield.target.value);
             newGrid(gridSize);
         }
+    })
+
+    blackBrush.addEventListener("click", () => {
+        const totalNoOfGrids = sketchContainer.childElementCount
+        newGrid(totalNoOfGrids**0.5)
+    })
+
+    rainbowBrush.addEventListener("click", () => {
+        const totalNoOfGrids = sketchContainer.childElementCount
+        newGrid(totalNoOfGrids**0.5)
     })
 }
 
